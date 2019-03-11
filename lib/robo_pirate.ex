771758aa86361848eb2be684b6584bee35fe7dd7
Benjamin <benjamin@rotendahl.dk>
2019-03-11 13:12:54 +0100

    Heroku port type fix

--- lib/robo_pirate.ex
+++ lib/robo_pirate.ex
@@ -3,12 +3,12 @@ defmodule RoboPirate do
 
   def start(_type, _args) do
     IO.inspect "PORT IS"
-    IO.inspect Application.get_env(:robo_pirate, :port)
+    {port, _} = Application.get_env(:robo_pirate, :port)
     children = [
       Plug.Adapters.Cowboy.child_spec(
         scheme: :http,
         plug: RoboPirate.Router,
-        options: [port: Application.get_env(:robo_pirate, :port)]
+        options: [port: port]
       )
     ]
 
